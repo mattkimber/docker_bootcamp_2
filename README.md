9d7185278b0c3855d9dc414d10cf6ca20e6376a6
Matt Kimber <matt.kimber@collinsongroup.com>
2018-08-20 09:13:17 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Docker Bootcamp 2
+
+This repository contains container image definitions for the Docker Bootcamp session
+on mount points and persistent state.
+
+It consists of a simple CRUD app allowing the user to upload photos, in two different
+versions to illustrate container upgrading concepts.
+
+This is probably not too useful outside the training, although you may wish to use
+the files as the basis for a simple Express app and/or Node Dockerfile.
+
---
+++ container_2.0/src/index.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const multer = require("multer");
+const fs = require("fs");
+const path = require('path');
+
+const storage = multer.diskStorage({
+  destination: (req, file, callback) => {callback(null, "files/")},
+  filename: (req, file, callback) => {callback(null, file.originalname)}
+});
+
+const upload = multer({storage: storage});
+
+var renderPage = (method, req, res) => {
+  console.log(method + " request from " + req.ip);
+  res.render(
+    "index", {
+      title: "Simple Photo Uploader",
+      header: "Photo Uploader",
+      images: getImages()
+    }
+  );
+};
+
+var getImages = () => {
+  let result = [];
+  let files = fs.readdirSync("files");
+
+  for(file of files) {
+    let location = path.join("files", file);
+    var stat = fs.statSync(location);
+    if(stat && stat.isFile() && [".jpg", ".png", ".gif", ".jpeg"].indexOf(path.extname(location) != -1)) {
+      result.push("/static/" + file);
+    }
+  }
+
+  return result;
+}
+
+var app = express();
+
+app.set("view engine", "pug");
+app.set("port", process.env.PORT || 3000);
+
+app.use('/static', express.static(path.join(__dirname, 'files')))
+
+app.get("/", (req, res) => {
+  renderPage("GET", req, res);
+});
+
+app.post("/", upload.single("fileupload"), (req, res) => {
+  renderPage("POST", req, res);
+});
+
+app.listen(app.get("port"), function() {
+  console.log("Web server started");
+});
